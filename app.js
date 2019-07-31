af28dbc89b990868f64dfb1278f773817506c89d
Jasmin <jasymatos@gmail.com>
2019-07-30 22:39:33 -0400

    added new files

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+//make file .env and add to gitignore file. holds API keys
+
+
 //1.  run `npm init -y` &mdash; this will initialize a `package.json` file for your project.
 
 //2. 
---
+++ keys.js
@@ -0,0 +1,16 @@
+console.log("Keys loaded");
+
+//commands:
+
+//spotify this song "A song"
+
+//concert this Dave Mathews band
+
+//do-what-it-says ----reads a file--- like a song ---
+
+
+//take screenshots and put it in the read me file for grading
+
+//mark down language for readmes
+
+//
